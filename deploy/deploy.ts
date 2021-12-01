c19741d9538ad606ac6cba1913b966e4f6696cf9
IlyaPrigogine <prigogine@protonmail.com>
2021-12-01 13:29:28 +0800

    submitted

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -4,7 +4,7 @@ import {DeployFunction} from 'hardhat-deploy/types';
 const func: DeployFunction = async function ({deployments, getNamedAccounts, network, getChainId}) {
     const {deploy,read,get,execute} = deployments;
     const {owner} = await getNamedAccounts();
-    
+
 };
 export default func;
 func.tags = ['Greeter'];
--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -3,13 +3,8 @@ import {deployments, getNamedAccounts} from 'hardhat';
 const {execute, read} = deployments;
 
 async function main() {
-
     const {owner} = await getNamedAccounts();
 
-    /* code of Greeter func*/
-    // console.log(`BEFORE ${await read('Greeter','greet')}`);
-    // await execute('Greeter',{from: owner}, 'setGreeting', 'A new greeting');
-    // console.log(`AFTER: ${await read('Greeter','greet')}`);
 
 }
 
