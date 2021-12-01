6423bd482d9d6b1433d6a314d0a1a94af2622c15
IlyaPrigogine <prigogine@protonmail.com>
2021-12-01 12:53:08 +0800

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -4,17 +4,7 @@ import {DeployFunction} from 'hardhat-deploy/types';
 const func: DeployFunction = async function ({deployments, getNamedAccounts, network, getChainId}) {
     const {deploy,read,get,execute} = deployments;
     const {owner} = await getNamedAccounts();
-
-    // console.log('chainId:', await getChainId());
-    //
-    // await deploy('Greeter', {
-    //     from: owner,
-    //     args: ['new greeting'],
-    //     log: true,
-    // });
-
-
-
+    
 };
 export default func;
 func.tags = ['Greeter'];
