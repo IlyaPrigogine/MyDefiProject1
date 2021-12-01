7bca2564b35e16ed2d54b1d132b1ef0830c3198a
IlyaPrigogine <prigogine@protonmail.com>
2021-12-01 13:59:23 +0800

    Update dai_funcs.ts

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -1,6 +1,7 @@
 import {deployments, getNamedAccounts} from 'hardhat';
 import {formatEther, parseEther} from "ethers/lib/utils";
 import {ethers} from "ethers";
+import {BUSD} from "../typechain";
 
 const {execute, read} = deployments;
 
@@ -8,16 +9,22 @@ async function main() {
     const {deploy, execute, get, read} = deployments;
     const {owner} = await getNamedAccounts();
 
-    const MyDefiProject = await get("MyDefiProject");
-    const BUSD = await get('BUSD');
+    //step 1:
+    // const MyDefiProject = await get("MyDefiProject");
+    // const BUSD = await get('BUSD');
+    //
+    // const busd_owner = await read('BUSD','balanceOf',owner);
+    // console.log(`busd.balanceOf(owner): ${formatEther(busd_owner)}`);
+    //
+    // await execute('BUSD',{from: owner, log: true}, 'approve',MyDefiProject.address,parseEther('10000'));
 
-    const busd_owner = await read('BUSD','balanceOf',owner);
-    console.log(`busd.balanceOf(owner): ${formatEther(busd_owner)}`);
+    //step 2
+    // console.log(`${(await get('BUSD')).address}`);
+    // await execute('MyDefiProject', {from: owner, log: true}, 'foo', ethers.constants.AddressZero,parseEther('1.23'));
+    //
+    // console.log(`busd.balanceOf(owner): ${formatEther(await read('BUSD','balanceOf',owner))}`);
 
-    await execute('BUSD',{from: owner, log: true}, 'approve',MyDefiProject.address,parseEther('10000'));
-    await execute('MyDefiProject', {from: owner, log: true}, 'foo', ethers.constants.AddressZero,parseEther('1.23'));
 
-    console.log(`busd.balanceOf(owner): ${formatEther(await read('BUSD','balanceOf',owner))}`);
 }
 
 
